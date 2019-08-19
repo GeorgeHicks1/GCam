9ffb512b0e1b777c3e2081d7542eca3cbabca33d
George Hicks <georgehicks88@gmail.com>
2019-08-19 12:12:23 +0100

    Revert "Reorganised files into folder system"
    This reverts commit a5a6ba3aeb6e7adc4b830d3615affedfad346aa3.

--- functions/settings/settings_button_press.m
+++
@@ -1,7 +0,0 @@
-function settings_button_press(pushbutton,~)
-    fig_camera=pushbutton.Parent;
-    fig_settings=fig_camera.UserData.fig_settings;
-    fig_settings.Visible='On';
-    figure(fig_settings); %brings the figure to the front
-    
-end
--- functions/settings/settings_window_close_request.m
+++
@@ -1,3 +0,0 @@
-function settings_window_close_request(settings_window,~)
-    settings_window.Visible='Off';
-end
